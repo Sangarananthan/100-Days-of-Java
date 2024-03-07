0facbb29f7f980326fbdfda993fd9a17496dd006
Sangarananthan <sangarsmart16@gmail.com>
2024-03-07 19:54:43 +0530

    commit

--- src/com/Concepts/Arrrays.java
+++ src/com/Concepts/Arrrays.java
@@ -1,2 +1,18 @@
-package com.Concepts;public class Arrrays {
+package com.Concepts;
+
+import java.util.Arrays;
+
+public class Arrrays {
+    public static void main(String[] args) {
+        int[] arr = new int[8];
+        System.out.println(
+                Arrays.toString(arr)
+        );
+
+        int[][] array = new int[3][3];
+        for(int[] i: array)
+        {
+            System.out.println(Arrays.toString(i));
+        }
+    }
 }

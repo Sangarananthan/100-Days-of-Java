63ef1ab3ee7331619f6c900effd74626512f4c02
Sangar <sangarsmart16@gmail.com>
2024-03-30 14:44:58 +0530

    commit

--- src/Java/Recursion/Find_all_index.java
+++
@@ -1,7 +0,0 @@
-package Java.Recursion;
-
-public class Find_all_index {
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
-    }
-}
---
+++ src/Java/Recursion/pattern_triangle.java
@@ -0,0 +1,21 @@
+package Java.Recursion;
+
+public class pattern_triangle {
+    public static void main(String[] args) {
+        triangle(4, 0);
+    }
+
+    public static void triangle(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (r > c) {
+            System.out.print("* ");
+            triangle(r, c + 1);
+        } else {
+            System.out.println();
+            triangle(r - 1, 0);
+        }
+    }
+
+}

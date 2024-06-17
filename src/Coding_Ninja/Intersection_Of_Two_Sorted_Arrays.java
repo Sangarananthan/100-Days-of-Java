29765dc624d5e5501b26c00a3043ea5f69c2ec8d
Sangar <sangarsmart16@gmail.com>
2024-06-17 08:35:41 +0530

    commit

--- src/Coding_Ninja/Intersection_Of_Two_Sorted_Arrays.java
+++
@@ -1,7 +0,0 @@
-package Coding_Ninja;
-//https://www.codingninjas.com/studio/problems/intersection-of-2-arrays_1082149
-public class Intersection_Of_Two_Sorted_Arrays {
-    public static void main(String[] args) {
-
-    }
-}
--- src/Java/Maths/odd_even.java
+++
@@ -1,8 +0,0 @@
-package Java.Maths;
-
-public class odd_even {
-    public static void main(String[] args) {
-        int n = 56;
-        System.out.println((n&1)==0);
-    }
-}
